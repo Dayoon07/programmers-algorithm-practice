ab1dd92b2e501c54697d0b97b517939247f3b5db
Dayoon07 <gangd0642@gmail.com>
2025-02-01 15:23:56 +0900

    이건 새로 푼 문제

--- ap/src/ap2/배열두배만들기.java
+++ ap/src/ap2/배열두배만들기.java
@@ -11,7 +11,7 @@ public class 배열두배만들기 {
 
     public int[] solution(int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] *= 2;
+            numbers[i] *= 2; 
         }
         return numbers;
     }
--- ap/src/ap2/분수의덧셈.java
+++ ap/src/ap2/분수의덧셈.java
@@ -19,7 +19,7 @@ public class 분수의덧셈 {
         return new int[]{numer / gcd, lcm / gcd};
     }
 
-    private int gcd(int a, int b) {
+    private int gcd(int a, int b) { 
         while (b != 0) {
             int temp = b;
             b = a % b;
